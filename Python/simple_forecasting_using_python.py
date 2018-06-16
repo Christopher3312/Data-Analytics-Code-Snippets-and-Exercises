0194df39e611332b706b7ea3d8ed61889f89e8cd
Christopher Goodfellow <christopher3312@gmail.com>
2018-06-16 18:07:32 +1000

    Add files via upload

---
+++ Python/simple_forecasting_using_python.py
@@ -0,0 +1,30 @@
+'''
+Utilising the fbprophet library, predictions using historical data can be made. 
+This simple script will return 3 seperate charts.
+'''
+
+from fbprophet import Prophet #pip install fbprophet
+
+import os
+import pandas as pd
+import numpy as np
+from fbprophet import Prophet
+import warnings
+warnings.filterwarnings("ignore")
+
+path_t5 = 'task_5.csv'
+data_t5 = pd.read_csv(path_t5)
+data_t5['ds'] = pd.to_datetime(data_t5['ds'])
+data_t5.set_index('ds').plot(figsize=(12, 9))
+
+
+def cast_prediction(arg_data):
+    m = Prophet()
+    m.fit(arg_data)
+    future_data = m.make_future_dataframe(periods=365)
+    forecast = m.predict(future_data)
+    m.plot(forecast);
+    
+cast_prediction(data_t5)
+cast_complex_prediction(data_t5)
+
